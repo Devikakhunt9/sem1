b1df6cbc5a3d387ff659088f89d201c6a84eac3d
Devikakhunt9 <104673812+Devikakhunt9@users.noreply.github.com>
2022-07-08 22:16:00 +0530

    Create 3_a.c

---
+++ c pro/Lab3/3_a.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+void main()
+{
+	printf("Hello World");
+}
+
+#include<stdio.h>
+void main()
+{
+	printf("\"NILKANTH\",\Juna ganesh nagar socity,\nNr.Balaji provision store,\nKothariya-Rajkot.")
+}
+
+#include<stdio.h>
+void main()
+{
+	printf("\"nilkanth\",\n");
+	printf("Juna ganesh nagar socity,\n");
+	printf("Nr.Balji provision store.\n");
+	
+}
+
+#include<stdio.h>
+void main()
+{
+	int a=2,b=4,add;
+	add=a+b;
+	printf("ans : %d + %d : %d",a,b,add);
+}
+
+#include<stdio.h>
+void main()
+{
+	int a=9,b=5,avg;
+	avg=(a+b)/2;
+	printf("ans : %d",avg);
+}
+
+#include<stdio.h>
+void main()
+{
+	int a,b,add;
+	printf("a:");
+	scanf("%d",&a);
+	printf("b :");
+	scanf("%d",&b);
+	add=a+b;
+	printf("ANS:%d",add);
+	
+}
